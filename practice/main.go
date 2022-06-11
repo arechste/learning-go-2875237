298231af479145e5d3a6397a3f076e6b7f3d1eb3
Alex Rechsteiner <769693+arechste@users.noreply.github.com>
2022-06-11 15:08:37 +0200

    structs stuff

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	poodle.Weight = 9
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+}
+
+// struct exported Dog / private dog
+//Dog is a struct
+type Dog struct {
+	//exported fields
+	Breed  string
+	Weight int
 }
