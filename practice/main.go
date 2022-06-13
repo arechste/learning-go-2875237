1819c47ad7ad7293cc9429c0240efb237a43d4c1
Alex Rechsteiner <769693+arechste@users.noreply.github.com>
2022-06-13 10:38:53 +0200

    file stuff

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello from Go!")
+	content := "Hello from Go!"
+	file, err := os.Create("./fromString.txt")
+	fmt.Println("Files")
+	checkError(err)
+
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("Wrote a file with %v characters\n", length)
+	defer file.Close()
+	defer readFile("./fromString.txt")
+}
+
+func readFile(fileName string) {
+	data, err := ioutil.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from file:", string(data))
+}
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+
 }
